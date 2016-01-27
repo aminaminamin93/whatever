a90f4dfc872a59ce28411f7dfd24722cddadbab2
aminaminamin93 <battlefieldcompny@gmail.com>
2016-01-26 17:21:17 -0800

    second commit

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -9,16 +9,16 @@
     <title>Dashboard - SB Admin</title>
 
     <!-- Bootstrap core CSS -->
-    {!! Html::style('bootstrap/css/bootstrap.css') !!}
+    {!! Html::style('/bootstrap/css/bootstrap.css') !!}
     
     <!-- Add custom CSS here -->
-    {!! Html::style('bootstrap/css/sb-admin.css') !!}
-    {!! Html::style('bootstrap/font-awesome/css/font-awesome.min.css') !!}
+    {!! Html::style('/bootstrap/css/sb-admin.css') !!}
+    {!! Html::style('/bootstrap/font-awesome/css/font-awesome.min.css') !!}
     
     <!-- Page Specific CSS -->
     <link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
-    {!! Html::script('bootstrap/js/jquery-1.10.2.js') !!}
-    {!! Html::script('bootstrap/js/bootstrap.js')!!}
+    {!! Html::script('/bootstrap/js/jquery-1.10.2.js') !!}
+    {!! Html::script('/bootstrap/js/bootstrap.js')!!}
     
   </head>
 
@@ -34,7 +34,7 @@
 
 
     <!-- Page Specific Plugins -->
-   
+
     <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
      {!! Html::script('http://cdn.oesmith.co.uk/morris-0.4.3.min.js') !!}
     {!! Html::script('bootstrap/js/morris/chart-data-morris.js') !!}
